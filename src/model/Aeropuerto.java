734f2c8210a908b5c1ef94324c138276886a84a0
Urtzi <urcrmedev@gmail.com>
2023-10-25 10:31:23 +0200

    UPDATE Aeropuerto.java class finished with getters setters and contructors.

--- src/model/Aeropuerto.java
+++ src/model/Aeropuerto.java
@@ -1,5 +1,65 @@
 package model;
 
 public class Aeropuerto {
+	int id;
+	String nombre;
+	int anio,capacidad,id_dir;
+	String imagen;
+	Direccion direccion;
+	public Aeropuerto(int id, String nombre, int anio, int capacidad, int id_dir, String imagen, Direccion direccion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.anio = anio;
+		this.capacidad = capacidad;
+		this.id_dir = id_dir;
+		this.imagen = imagen;
+		this.direccion = direccion;
+	}
+	public Aeropuerto(Aeropuerto aeropuerto) {
+		super();
+		this.id = aeropuerto.id;
+		this.nombre = aeropuerto.nombre;
+		this.anio = aeropuerto.anio;
+		this.capacidad = aeropuerto.capacidad;
+		this.id_dir = aeropuerto.id_dir;
+		this.imagen = aeropuerto.imagen;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+	public int getCapacidad() {
+		return capacidad;
+	}
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+	public int getId_dir() {
+		return id_dir;
+	}
+	public void setId_dir(int id_dir) {
+		this.id_dir = id_dir;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
 	
 }
