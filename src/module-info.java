e0da7a529b4e9e8471c96bd45a0956d3a46a089e
Urtzi <urcrmedev@gmail.com>
2023-10-25 11:49:15 +0200

    ADD Added dependencies to module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,9 @@
  */
 module EjercicioL {
 	requires java.sql;
+	requires javafx.graphics;
+	requires javafx.fxml;
+    requires javafx.controls;
+	requires javafx.base;
+	opens application to javafx.base, javafx.graphics, javafx.fxml;
 }
