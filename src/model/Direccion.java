122ad6d04e90c562cc118c1c3694a92020e5a8fb
Urtzi <urcrmedev@gmail.com>
2023-10-25 10:37:58 +0200

    UPDATE Direccion.java clas, added constructors and get/set methods

--- src/model/Direccion.java
+++ src/model/Direccion.java
@@ -1,5 +1,23 @@
 package model;
 
 public class Direccion {
-
+	private int id;
+	private String pais,ciudad,calle;
+	private int numero;
+	public Direccion(int id, String pais, String ciudad, String calle, int numero) {
+		super();
+		this.id = id;
+		this.pais = pais;
+		this.ciudad = ciudad;
+		this.calle = calle;
+		this.numero = numero;
+	}
+	public Direccion(String pais, String ciudad, String calle, int numero) {
+		super();
+		this.id = -1;
+		this.pais = pais;
+		this.ciudad = ciudad;
+		this.calle = calle;
+		this.numero = numero;
+	}
 }
